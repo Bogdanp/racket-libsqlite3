5d77168957bcb820c9e113bb55994a3b895f5faf
Bogdan Popa <bogdan@defn.io>
2020-05-24 16:15:28 +0300

    build: bump versions

--- libsqlite3-x86_64-linux/info.rkt
+++ libsqlite3-x86_64-linux/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
-(define version "3.29.0")
+(define version "3.32.0")
 (define deps '("base"))
 (define copy-foreign-libs '("libsqlite3.so"))
 (define install-platform #rx"x86_64-linux")
