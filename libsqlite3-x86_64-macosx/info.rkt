a78bea8f9b088d5c8fe2d60021100729aa3a1552
Bogdan Popa <bogdan@defn.io>
2021-04-29 07:56:59 +0300

    core: bump version to 3.35.5

--- libsqlite3-x86_64-macosx/info.rkt
+++ libsqlite3-x86_64-macosx/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
-(define version "3.32.0")
+(define version "3.35.5")
 (define deps '("base"))
 (define copy-foreign-libs '("libsqlite3.dylib"))
 (define install-platform #rx"x86_64-macosx")
