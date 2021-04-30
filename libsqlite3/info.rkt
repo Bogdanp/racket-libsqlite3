9d3c90f118af22937aea03e47949142d2afb013b
Bogdan Popa <bogdan@defn.io>
2021-04-30 10:41:16 +0300

    libsqlite3: drop build deps

--- libsqlite3/info.rkt
+++ libsqlite3/info.rkt
@@ -5,7 +5,3 @@
                ("libsqlite3-x86_64-linux" #:platform #rx"x86_64-linux")
                ("libsqlite3-x86_64-macosx" #:platform #rx"x86_64-macosx")
                ("libsqlite3-x86_64-win32" #:platform #rx"win32.x86_64")))
-(define build-deps '(("libsqlite3-i386-win32" #:platform #rx"win32.i386")
-                     ("libsqlite3-x86_64-linux" #:platform #rx"x86_64-linux")
-                     ("libsqlite3-x86_64-macosx" #:platform #rx"x86_64-macosx")
-                     ("libsqlite3-x86_64-win32" #:platform #rx"win32.x86_64")))
