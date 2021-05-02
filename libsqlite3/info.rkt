cdb53b7c914f304620753233eccbc86d50b92a39
Bogdan Popa <bogdan@defn.io>
2021-05-02 08:45:09 +0300

    build: add dep on base

--- libsqlite3/info.rkt
+++ libsqlite3/info.rkt
@@ -1,7 +1,8 @@
 #lang info
 
 (define version "3.35.5")
-(define deps '(("libsqlite3-i386-win32" #:platform #rx"win32.i386")
+(define deps '("base"
+               ("libsqlite3-i386-win32" #:platform #rx"win32.i386")
                ("libsqlite3-x86_64-linux" #:platform #rx"x86_64-linux")
                ("libsqlite3-x86_64-macosx" #:platform #rx"x86_64-macosx")
                ("libsqlite3-x86_64-win32" #:platform #rx"win32.x86_64")))
