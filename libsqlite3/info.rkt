ffb6492687d9b3315649d35f8f581422f45f3e03
Bogdan Popa <bogdan@defn.io>
2021-07-21 07:37:09 +0300

    core: add dep on libsqlite3-aarch64-macosx for arm64 [skip ci]

--- libsqlite3/info.rkt
+++ libsqlite3/info.rkt
@@ -2,6 +2,7 @@
 
 (define version "3.36.0")
 (define deps '("base"
+               ("libsqlite3-aarch64-macosx" #:platform #rx"aarch64-macosx")
                ("libsqlite3-i386-win32" #:platform #rx"win32.i386")
                ("libsqlite3-x86_64-linux" #:platform #rx"x86_64-linux")
                ("libsqlite3-x86_64-macosx" #:platform #rx"x86_64-macosx")
